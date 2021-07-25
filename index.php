7fed091003c1b970023a6b38cb17f4cf48180265
Galina <87332010+Lexsa2021@users.noreply.github.com>
2021-07-25 10:02:47 +0300

    Add files via upload
    Created table of the multiplication

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
 ﻿<!DOCTYPE html>
 <html>
-<head><title>Start lessons</title></head>
+	<head>
+		<meta charset='utf-8'>
+		<title>My Site</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
 <body>
-<?php
-echo "Hello, <b>NIX Education</b>"
-?>
+	<header class="top">
+		<h1 class="logo">My first site</h1>
+	</header>
+	<nav>
+		<a href="index.php">Home</a> | <a href="https://github.com/Lexsa2021/mysite.local.git">GitHub</a> | <a href="lesson1.php">Lesson 1</a> | <a href="#">Lesson 2 </a>
+	</nav>
+	<section class="main">
+	<h1>What is Lorem Ipsum?</h1>
+ <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+ Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+ when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+ It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+ It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
+ with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+	</section>
+<footer>
+&copy2021
+</footer>
 </body>
 </html>
---
+++ lesson1.php
@@ -0,0 +1,567 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<title>My Site</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+<body>
+	<header class="top">
+		<h1 class="logo">My first site</h1>
+	</header>
+	<nav>
+		<a href="index.php">Home</a> | <a href="https://github.com/Lexsa2021/mysite.local.git">GitHub</a> | <a href="lesson1.php">Lesson 1</a> | <a href="#">Lesson 2 </a>
+	</nav>
+	<section class="main">
+	<h1 align="center">Таблица умножений</h1>
+<?php
+$a=1;
+$b=2;
+$c=3;
+$d=4;
+$e=5;
+$f=6;
+$g=7;
+$h=8;
+$i=9;
+$j=10;
+function mult1_1($a){
+	$mult1_1=$a*$a;
+	return $mult1_1;
+}
+function mult1_2($a,$b){
+	$mult1_2=$a*$b;
+	return $mult1_2;
+}
+function mult1_3($a,$c){
+	$mult1_3=$a*$c;
+	return $mult1_3;
+}
+function mult1_4($a,$d){
+	$mult1_4=$a*$d;
+	return $mult1_4;
+}
+function mult1_5($a,$e){
+	$mult1_5=$a*$e;
+	return $mult1_5;
+}
+function mult1_6($a,$f){
+	$mult1_6=$a*$f;
+	return $mult1_6;
+}
+function mult1_7($a,$g){
+	$mult1_7=$a*$g;
+	return $mult1_7;
+}
+function mult1_8($a,$h){
+	$mult1_8=$a*$h;
+	return $mult1_8;
+}
+function mult1_9($a,$i){
+	$mult1_9=$a*$i;
+	return $mult1_9;
+}
+function mult1_10($a,$j){
+	$mult1_10=$a*$j;
+	return $mult1_10;
+}
+$rez1_1=mult1_1(1);
+$rez1_2=mult1_2(1,2);
+$rez1_3=mult1_3(1,3);
+$rez1_4=mult1_4(1,4);
+$rez1_5=mult1_5(1,5);
+$rez1_6=mult1_6(1,6);
+$rez1_7=mult1_7(1,7);
+$rez1_8=mult1_8(1,8);
+$rez1_9=mult1_9(1,9);
+$rez1_10=mult1_10(1,10);
+function mult2_1($b,$a){
+	$mult2_1=$b*$a;
+	return $mult2_1;
+}
+function mult2_2($b){
+	$mult2_2=$b*$b;
+	return $mult2_2;
+}
+function mult2_3($b,$c){
+	$mult2_3=$b*$c;
+	return $mult2_3;
+}
+function mult2_4($b,$d){
+	$mult2_4=$b*$d;
+	return $mult2_4;
+}
+function mult2_5($b,$e){
+	$mult2_5=$b*$e;
+	return $mult2_5;
+}
+function mult2_6($b,$f){
+	$mult2_6=$b*$f;
+	return $mult2_6;
+}
+function mult2_7($b,$g){
+	$mult2_7=$b*$g;
+	return $mult2_7;
+}
+function mult2_8($b,$h){
+	$mult2_8=$b*$h;
+	return $mult2_8;
+}
+function mult2_9($b,$i){
+	$mult2_9=$b*$i;
+	return $mult2_9;
+}
+function mult2_10($b,$j){
+	$mult2_10=$b*$j;
+	return $mult2_10;
+}
+$rez2_1=mult2_1(2,1);
+$rez2_2=mult2_2(2,2);
+$rez2_3=mult2_3(2,3);
+$rez2_4=mult2_4(2,4);
+$rez2_5=mult2_5(2,5);
+$rez2_6=mult2_6(2,6);
+$rez2_7=mult2_7(2,7);
+$rez2_8=mult2_8(2,8);
+$rez2_9=mult2_9(2,9);
+$rez2_10=mult2_10(2,10);
+function mult3_1($c,$a){
+	$mult3_1=$c*$a;
+	return $mult3_1;
+}
+function mult3_2($c,$b){
+	$mult3_2=$c*$b;
+	return $mult3_2;
+}
+function mult3_3($c){
+	$mult3_3=$c*$c;
+	return $mult3_3;
+}
+function mult3_4($c,$d){
+	$mult3_4=$c*$d;
+	return $mult3_4;
+}
+function mult3_5($c,$e){
+	$mult3_5=$c*$e;
+	return $mult3_5;
+}
+function mult3_6($c,$f){
+	$mult3_6=$c*$f;
+	return $mult3_6;
+}
+function mult3_7($c,$g){
+	$mult3_7=$c*$g;
+	return $mult3_7;
+}
+function mult3_8($c,$h){
+	$mult3_8=$c*$h;
+	return $mult3_8;
+}
+function mult3_9($c,$i){
+	$mult3_9=$c*$i;
+	return $mult3_9;
+}
+function mult3_10($c,$j){
+	$mult3_10=$c*$j;
+	return $mult3_10;
+}
+$rez3_1=mult3_1(3,1);
+$rez3_2=mult3_2(3,2);
+$rez3_3=mult3_3(3,3);
+$rez3_4=mult3_4(3,4);
+$rez3_5=mult3_5(3,5);
+$rez3_6=mult3_6(3,6);
+$rez3_7=mult3_7(3,7);
+$rez3_8=mult3_8(3,8);
+$rez3_9=mult3_9(3,9);
+$rez3_10=mult3_10(3,10);
+function mult4_1($d,$a){
+	$mult4_1=$d*$a;
+	return $mult4_1;
+}
+function mult4_2($d,$b){
+	$mult4_2=$d*$b;
+	return $mult4_2;
+}
+function mult4_3($d,$c){
+	$mult4_3=$d*$c;
+	return $mult4_3;
+}
+function mult4_4($d){
+	$mult4_4=$d*$d;
+	return $mult4_4;
+}
+function mult4_5($d,$e){
+	$mult4_5=$d*$e;
+	return $mult4_5;
+}
+function mult4_6($d,$f){
+	$mult4_6=$d*$f;
+	return $mult4_6;
+}
+function mult4_7($d,$g){
+	$mult4_7=$d*$g;
+	return $mult4_7;
+}
+function mult4_8($d,$h){
+	$mult4_8=$d*$h;
+	return $mult4_8;
+}
+function mult4_9($d,$i){
+	$mult4_9=$d*$i;
+	return $mult4_9;
+}
+function mult4_10($d,$j){
+	$mult4_10=$d*$j;
+	return $mult4_10;
+}
+$rez4_1=mult4_1(4,1);
+$rez4_2=mult4_2(4,2);
+$rez4_3=mult4_3(4,3);
+$rez4_4=mult4_4(4,4);
+$rez4_5=mult4_5(4,5);
+$rez4_6=mult4_6(4,6);
+$rez4_7=mult4_7(4,7);
+$rez4_8=mult4_8(4,8);
+$rez4_9=mult4_9(4,9);
+$rez4_10=mult4_10(4,10);
+function mult5_1($e,$a){
+	$mult5_1=$e*$a;
+	return $mult5_1;
+}
+function mult5_2($e,$b){
+	$mult5_2=$e*$b;
+	return $mult5_2;
+}
+function mult5_3($e,$c){
+	$mult5_3=$e*$c;
+	return $mult5_3;
+}
+function mult5_4($e,$d){
+	$mult5_4=$e*$d;
+	return $mult5_4;
+}
+function mult5_5($e){
+	$mult5_5=$e*$e;
+	return $mult5_5;
+}
+function mult5_6($e,$f){
+	$mult5_6=$e*$f;
+	return $mult5_6;
+}
+function mult5_7($e,$g){
+	$mult5_7=$e*$g;
+	return $mult5_7;
+}
+function mult5_8($e,$h){
+	$mult5_8=$e*$h;
+	return $mult5_8;
+}
+function mult5_9($e,$i){
+	$mult5_9=$e*$i;
+	return $mult5_9;
+}
+function mult5_10($e,$j){
+	$mult5_10=$e*$j;
+	return $mult5_10;
+}
+$rez5_1=mult5_1(5,1);
+$rez5_2=mult5_2(5,2);
+$rez5_3=mult5_3(5,3);
+$rez5_4=mult5_4(5,4);
+$rez5_5=mult5_5(5,5);
+$rez5_6=mult5_6(5,6);
+$rez5_7=mult5_7(5,7);
+$rez5_8=mult5_8(5,8);
+$rez5_9=mult5_9(5,9);
+$rez5_10=mult5_10(5,10);
+function mult6_1($f,$a){
+	$mult6_1=$f*$a;
+	return $mult6_1;
+}
+function mult6_2($f,$b){
+	$mult6_2=$f*$b;
+	return $mult6_2;
+}
+function mult6_3($f,$c){
+	$mult6_3=$f*$c;
+	return $mult6_3;
+}
+function mult6_4($f,$d){
+	$mult6_4=$f*$d;
+	return $mult6_4;
+}
+function mult6_5($f,$e){
+	$mult6_5=$f*$e;
+	return $mult6_5;
+}
+function mult6_6($f){
+	$mult6_6=$f*$f;
+	return $mult6_6;
+}
+function mult6_7($f,$g){
+	$mult6_7=$f*$g;
+	return $mult6_7;
+}
+function mult6_8($f,$h){
+	$mult6_8=$f*$h;
+	return $mult6_8;
+}
+function mult6_9($f,$i){
+	$mult6_9=$f*$i;
+	return $mult6_9;
+}
+function mult6_10($f,$j){
+	$mult6_10=$f*$j;
+	return $mult6_10;
+}
+$rez6_1=mult6_1(6,1);
+$rez6_2=mult6_2(6,2);
+$rez6_3=mult6_3(6,3);
+$rez6_4=mult6_4(6,4);
+$rez6_5=mult6_5(6,5);
+$rez6_6=mult6_6(6,6);
+$rez6_7=mult6_7(6,7);
+$rez6_8=mult6_8(6,8);
+$rez6_9=mult6_9(6,9);
+$rez6_10=mult6_10(6,10);
+function mult7_1($g,$a){
+	$mult7_1=$g*$a;
+	return $mult7_1;
+}
+function mult7_2($g,$b){
+	$mult7_2=$g*$b;
+	return $mult7_2;
+}
+function mult7_3($g,$c){
+	$mult7_3=$g*$c;
+	return $mult7_3;
+}
+function mult7_4($g,$d){
+	$mult7_4=$g*$d;
+	return $mult7_4;
+}
+function mult7_5($g,$e){
+	$mult7_5=$g*$e;
+	return $mult7_5;
+}
+function mult7_6($g,$f){
+	$mult7_6=$g*$f;
+	return $mult7_6;
+}
+function mult7_7($g){
+	$mult7_7=$g*$g;
+	return $mult7_7;
+}
+function mult7_8($g,$h){
+	$mult7_8=$g*$h;
+	return $mult7_8;
+}
+function mult7_9($g,$i){
+	$mult7_9=$g*$i;
+	return $mult7_9;
+}
+function mult7_10($g,$j){
+	$mult7_10=$g*$j;
+	return $mult7_10;
+}
+$rez7_1=mult7_1(7,1);
+$rez7_2=mult7_2(7,2);
+$rez7_3=mult7_3(7,3);
+$rez7_4=mult7_4(7,4);
+$rez7_5=mult7_5(7,5);
+$rez7_6=mult7_6(7,6);
+$rez7_7=mult7_7(7,7);
+$rez7_8=mult7_8(7,8);
+$rez7_9=mult7_9(7,9);
+$rez7_10=mult7_10(7,10);
+function mult8_1($h,$a){
+	$mult8_1=$h*$a;
+	return $mult8_1;
+}
+function mult8_2($h,$b){
+	$mult8_2=$h*$b;
+	return $mult8_2;
+}
+function mult8_3($h,$c){
+	$mult8_3=$h*$c;
+	return $mult8_3;
+}
+function mult8_4($h,$d){
+	$mult8_4=$h*$d;
+	return $mult8_4;
+}
+function mult8_5($h,$e){
+	$mult8_5=$h*$e;
+	return $mult8_5;
+}
+function mult8_6($h,$f){
+	$mult8_6=$h*$f;
+	return $mult8_6;
+}
+function mult8_7($h,$g){
+	$mult8_7=$h*$g;
+	return $mult8_7;
+}
+function mult8_8($h){
+	$mult8_8=$h*$h;
+	return $mult8_8;
+}
+function mult8_9($h,$i){
+	$mult8_9=$h*$i;
+	return $mult8_9;
+}
+function mult8_10($h,$j){
+	$mult8_10=$h*$j;
+	return $mult8_10;
+}
+$rez8_1=mult8_1(8,1);
+$rez8_2=mult8_2(8,2);
+$rez8_3=mult8_3(8,3);
+$rez8_4=mult8_4(8,4);
+$rez8_5=mult8_5(8,5);
+$rez8_6=mult8_6(8,6);
+$rez8_7=mult8_7(8,7);
+$rez8_8=mult8_8(8,8);
+$rez8_9=mult8_9(8,9);
+$rez8_10=mult8_10(8,10);
+function mult9_1($i,$a){
+	$mult9_1=$i*$a;
+	return $mult9_1;
+}
+function mult9_2($i,$b){
+	$mult9_2=$i*$b;
+	return $mult9_2;
+}
+function mult9_3($i,$c){
+	$mult9_3=$i*$c;
+	return $mult9_3;
+}
+function mult9_4($i,$d){
+	$mult9_4=$i*$d;
+	return $mult9_4;
+}
+function mult9_5($i,$e){
+	$mult9_5=$i*$e;
+	return $mult9_5;
+}
+function mult9_6($i,$f){
+	$mult9_6=$i*$f;
+	return $mult9_6;
+}
+function mult9_7($i,$g){
+	$mult9_7=$i*$g;
+	return $mult9_7;
+}
+function mult9_8($i,$h){
+	$mult9_8=$i*$h;
+	return $mult9_8;
+}
+function mult9_9($i){
+	$mult9_9=$i*$i;
+	return $mult9_9;
+}
+function mult9_10($i,$j){
+	$mult9_10=$h*$j;
+	return $mult9_10;
+}
+$rez9_1=mult9_1(9,1);
+$rez9_2=mult9_2(9,2);
+$rez9_3=mult9_3(9,3);
+$rez9_4=mult9_4(9,4);
+$rez9_5=mult9_5(9,5);
+$rez9_6=mult9_6(9,6);
+$rez9_7=mult9_7(9,7);
+$rez9_8=mult9_8(9,8);
+$rez9_9=mult9_9(9,9);
+$rez9_10=mult9_10(9,10);
+function mult10_1($j,$a){
+	$mult10_1=$j*$a;
+	return $mult10_1;
+}
+function mult10_2($j,$b){
+	$mult10_2=$j*$b;
+	return $mult10_2;
+}
+function mult10_3($j,$c){
+	$mult10_3=$j*$c;
+	return $mult10_3;
+}
+function mult10_4($j,$d){
+	$mult10_4=$j*$d;
+	return $mult10_4;
+}
+function mult10_5($j,$e){
+	$mult10_5=$j*$e;
+	return $mult10_5;
+}
+function mult10_6($j,$f){
+	$mult10_6=$j*$f;
+	return $mult10_6;
+}
+function mult10_7($j,$g){
+	$mult10_7=$j*$g;
+	return $mult10_7;
+}
+function mult10_8($j,$h){
+	$mult10_8=$j*$h;
+	return $mult10_8;
+}
+function mult10_9($j,$i){
+	$mult10_9=$j*$i;
+	return $mult10_9;
+}
+function mult10_10($j){
+	$mult10_10=$j*$j;
+	return $mult10_10;
+}
+$rez10_1=mult10_1(10,1);
+$rez10_2=mult10_2(10,2);
+$rez10_3=mult10_3(10,3);
+$rez10_4=mult10_4(10,4);
+$rez10_5=mult10_5(10,5);
+$rez10_6=mult10_6(10,6);
+$rez10_7=mult10_7(10,7);
+$rez10_8=mult10_8(10,8);
+$rez10_9=mult10_9(10,9);
+$rez10_10=mult10_10(10,10);
+echo "<table border=\"1\">\n";
+echo "<tr><td>" .$a."*".$a."=".$rez1_1. "<br>" .$a."*".$b."=".$rez1_2.  "<br>" .$a. "*" .$c. "=" .$rez1_3."<br>" .$a. "*" .$d. "=" .$rez1_4. 
+"<br>" .$a. "*" .$e. "=" .$rez1_5. "<br>" .$a. "*" .$f. "=" .$rez1_6. "<br>" .$a. "*" .$g. "=" .$rez1_7. "<br>" .$a. "*" .$h. "=" .$rez1_8. "<br>"
+.$a. "*" .$i. "=" .$rez1_9. "<br>" .$a. "*" .$j. "=" .$rez1_10."</td><td>"
+.$b."*".$a."=".$rez2_1. "<br>".$b."*".$b."=".$rez2_2. "<br>" .$b."*".$c."=".$rez2_3.  "<br>" .$b. "*" .$d. "=" .$rez2_4. 
+"<br>" .$b. "*" .$e. "=" .$rez2_5. "<br>" .$b. "*" .$f. "=" .$rez2_6. "<br>" .$b. "*" .$g. "=" .$rez2_7. "<br>" .$b. "*" .$h. "=" .$rez2_8. "<br>"
+.$b. "*" .$i. "=" .$rez2_9. "<br>" .$b. "*" .$j. "=" .$rez2_10."</td><td>"
+.$c."*".$a."=".$rez3_1. "<br>".$c."*".$b."=".$rez3_2. "<br>" .$c."*".$c."=".$rez3_3.  "<br>" .$c. "*" .$d. "=" .$rez3_4. 
+"<br>" .$c. "*" .$e. "=" .$rez3_5. "<br>" .$c. "*" .$f. "=" .$rez3_6. "<br>" .$c. "*" .$g. "=" .$rez3_7. "<br>" .$c. "*" .$h. "=" .$rez3_8. "<br>"
+.$c. "*" .$i. "=" .$rez3_9. "<br>" .$c. "*" .$j. "=" .$rez3_10."</td><td>"
+.$d."*".$a."=".$rez4_1. "<br>".$d."*".$b."=".$rez4_2. "<br>" .$d."*".$c."=".$rez4_3.  "<br>" .$d. "*" .$d. "=" .$rez4_4. 
+"<br>" .$d. "*" .$e. "=" .$rez4_5. "<br>" .$d. "*" .$f. "=" .$rez4_6. "<br>" .$d. "*" .$g. "=" .$rez4_7. "<br>" .$d. "*" .$h. "=" .$rez4_8. "<br>"
+.$d. "*" .$i. "=" .$rez4_9. "<br>" .$d. "*" .$j. "=" .$rez4_10."</td><td>"
+.$e."*".$a."=".$rez5_1. "<br>".$e."*".$b."=".$rez5_2. "<br>" .$e."*".$c."=".$rez5_3.  "<br>" .$e. "*" .$d. "=" .$rez5_4. 
+"<br>" .$e. "*" .$e. "=" .$rez5_5. "<br>" .$e. "*" .$f. "=" .$rez5_6. "<br>" .$e. "*" .$g. "=" .$rez5_7. "<br>" .$e. "*" .$h. "=" .$rez5_8. "<br>"
+.$e. "*" .$i. "=" .$rez5_9. "<br>" .$e. "*" .$j. "=" .$rez5_10."</td></tr>";
+echo "<tr><td>" .$f."*".$a."=".$rez6_1. "<br>".$f."*".$b."=".$rez6_2. "<br>" .$f."*".$c."=".$rez6_3.  "<br>" .$f. "*" .$d. "=" .$rez6_4. 
+"<br>" .$f. "*" .$e. "=" .$rez6_5. "<br>" .$f. "*" .$f. "=" .$rez6_6. "<br>" .$f. "*" .$g. "=" .$rez6_7. "<br>" .$f. "*" .$h. "=" .$rez6_8. "<br>"
+.$f. "*" .$i. "=" .$rez6_9. "<br>" .$f. "*" .$j. "=" .$rez6_10."</td><td>"
+.$g."*".$a."=".$rez7_1. "<br>".$g."*".$b."=".$rez7_2. "<br>" .$g."*".$c."=".$rez7_3.  "<br>" .$g. "*" .$d. "=" .$rez7_4. 
+"<br>" .$g. "*" .$e. "=" .$rez7_5. "<br>" .$g. "*" .$f. "=" .$rez7_6. "<br>" .$g. "*" .$g. "=" .$rez7_7. "<br>" .$g. "*" .$h. "=" .$rez7_8. "<br>"
+.$g. "*" .$i. "=" .$rez7_9. "<br>" .$g. "*" .$j. "=" .$rez7_10."</td><td>"
+.$h."*".$a."=".$rez8_1. "<br>".$h."*".$b."=".$rez8_2. "<br>" .$h."*".$c."=".$rez8_3.  "<br>" .$h. "*" .$d. "=" .$rez8_4. 
+"<br>" .$h. "*" .$e. "=" .$rez8_5. "<br>" .$h. "*" .$f. "=" .$rez8_6. "<br>" .$h. "*" .$g. "=" .$rez8_7. "<br>" .$h. "*" .$h. "=" .$rez8_8. "<br>"
+.$h. "*" .$i. "=" .$rez8_9. "<br>" .$h. "*" .$j. "=" .$rez8_10."</td><td>"
+.$i."*".$a."=".$rez9_1. "<br>".$i."*".$b."=".$rez9_2. "<br>" .$i."*".$c."=".$rez9_3.  "<br>" .$i. "*" .$d. "=" .$rez9_4. 
+"<br>" .$i. "*" .$e. "=" .$rez9_5. "<br>" .$i. "*" .$f. "=" .$rez9_6. "<br>" .$i. "*" .$g. "=" .$rez9_7. "<br>" .$i. "*" .$h. "=" .$rez9_8. "<br>"
+.$i. "*" .$i. "=" .$rez9_9. "<br>" .$i. "*" .$j. "=" .$rez9_10."</td><td>"
+.$j."*".$a."=".$rez10_1. "<br>".$j."*".$b."=".$rez10_2. "<br>" .$j."*".$c."=".$rez10_3.  "<br>" .$j. "*" .$d. "=" .$rez10_4. 
+"<br>" .$j. "*" .$e. "=" .$rez10_5. "<br>" .$j. "*" .$f. "=" .$rez10_6. "<br>" .$j. "*" .$g. "=" .$rez10_7. "<br>" .$j. "*" .$h. "=" .$rez10_8. "<br>"
+.$j. "*" .$i. "=" .$rez10_9. "<br>" .$j. "*" .$j. "=" .$rez10_10.
+"</td></tr>";
+echo "</table>" 
+?>
+	</section>
+<footer>
+&copy2021
+</footer>
+</body>
+</html>
