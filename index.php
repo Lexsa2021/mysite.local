4af298008b6ea39469e0ef0acd6ca43e81a5b920
Galina <87332010+Lexsa2021@users.noreply.github.com>
2021-07-25 10:04:20 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -13,13 +13,7 @@
 		<a href="index.php">Home</a> | <a href="https://github.com/Lexsa2021/mysite.local.git">GitHub</a> | <a href="lesson1.php">Lesson 1</a> | <a href="#">Lesson 2 </a>
 	</nav>
 	<section class="main">
-	<h1>What is Lorem Ipsum?</h1>
- <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
- Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
- when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
- It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
- It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
- with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+	<h1>Hello, NIX Education</h1>
 	</section>
 <footer>
 &copy2021
