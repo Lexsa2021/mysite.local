33e2eba868190e0ddb8245a2a05b48d600e8dee6
Galina <87332010+Lexsa2021@users.noreply.github.com>
2021-09-10 20:27:27 +0300

    Add files via upload

---
+++ lesson3.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE html!>
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<title>Урок 3. Форма для отправления сообщения</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+<body>
+	<header class="top">
+		<h1 class="logo">My first site</h1>
+	</header>
+	<nav>
+		<a href="index.php">Home</a> | <a href="https://github.com/Lexsa2021/mysite.local.git">GitHub</a> | <a href="lesson1.php">Lesson 1</a> | <a href="lesson2.php">Lesson 2 </a> | <a href="lesson3.php">Lesson 3</a>
+	</nav>
+	<section class="main">
+	<h1 align="center">Введите любой текст</h1>
+<div>
+<form method="post" action="#">
+	  <input type="text" id="text" text="text"/>
+	  <button type="submit">Отправить</button></br>
+</form>
+</div>
+	</section>
+<footer>
+&copy2021
+</footer>
+</body>
+</html>
