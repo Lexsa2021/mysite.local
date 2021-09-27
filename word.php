0baafe519d3af915593b91c0933a493f794d2da8
Galina <87332010+Lexsa2021@users.noreply.github.com>
2021-09-27 20:10:00 +0300

    Add files via upload

---
+++ word.php
@@ -0,0 +1,27 @@
+﻿<!DOCTYPE html!>
+<html lang="ru">
+	<head>
+		<meta charset='utf-8'>
+		<title>Результат</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+<body>
+	<header class="top">
+		<h1 class="logo">My first site</h1>
+	</header>
+	<nav>
+		<a href="index.php">Home</a> | <a href="https://github.com/Lexsa2021/mysite.local.git">GitHub</a> | <a href="lesson1.php">Lesson 1</a> | <a href="lesson2.php">Lesson 2 </a> | <a href="lesson3.php">Lesson 3</a>
+	</nav>
+	<section class="main">
+	<h1 align="center">Введите любой текст</h1>
+<?php
+	$word = $_POST['word'];
+	echo strrev($word);
+?>
+	</section>
+<footer>
+&copy2021
+</footer>
+</body>
+</html>
+
